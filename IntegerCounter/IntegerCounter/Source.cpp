af36b723591c4d59570abfe9737185b9c3181ac2
razmathegreat <flatley.art@gmail.com>
2016-10-04 22:08:54 -0500

    Updated source file to read csv.

--- IntegerCounter/IntegerCounter/Source.cpp
+++ IntegerCounter/IntegerCounter/Source.cpp
@@ -1,10 +1,22 @@
 #include <iostream>;
+#include <fstream>;
+#include <string>;
 using namespace std;
 
-int main() {
-	int i;
-	cout << "Hello World";
-	cin >> i;
 
-	return 0;
+int main() {
+	int i=0;
+	string line;
+	ifstream file("./resources/test.csv");
+	if (file.is_open()) {
+		while (i < 100){
+			getline(file, line);
+			cout << line << '\n';
+			i++;
+		}
+		file.close();
+	}
+	else cout << "Unable to open file";
+	cin >> i;
+	return 0; 
 }
